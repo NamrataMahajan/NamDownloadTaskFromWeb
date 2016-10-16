de45f4016bbe732554ec0eab7d48c8cf7b8500cb
Namrata Mahajan <nammahajan12@gmail.com>
2016-10-16 23:54:40 +0530

    Initial Commit

---
+++ NamDownloadFromWeb/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NamDownloadFromWeb
+//
+//  Created by Namrata on 16/10/16.
+//  Copyright © 2016 Namrata Mahajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
